a8c61d620c4a54f14095ba5d4f27f4ed101d6f56
moatazeldebsy <moataznabil2019@gmail.com>
2022-07-02 23:22:37 +0200

    fix app

--- app/build/generated/ap_generated_sources/debug/out/com/insecureshop/BR.java
+++
@@ -1,5 +0,0 @@
-package com.insecureshop;
-
-public class BR {
-  public static final int _all = 0;
-}
--- app/build/generated/data_binding_trigger/debug/com/insecureshop/DataBindingTriggerClass.java
+++
@@ -1,4 +0,0 @@
-package com.insecureshop;
-
-@androidx.databinding.BindingBuildInfo
-public class DataBindingTriggerClass {}
